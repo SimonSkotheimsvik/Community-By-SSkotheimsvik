ae00986702d0be6425228d3d9c18020f895a59b1
Simon Skotheimsvik <simon.skotheimsvik@gmail.com>
2022-12-20 11:23:17 +0100

    Update Win11-EnableAdobeAcrobatForMicrosoftSensitivityLabels.ps1

--- Microsoft/Intune/Win11-EnableAdobeAcrobatForMicrosoftSensitivityLabels.ps1
+++ Microsoft/Intune/Win11-EnableAdobeAcrobatForMicrosoftSensitivityLabels.ps1
@@ -22,6 +22,7 @@
 #region - Get information about signed in user. 
 # Routine inspired by Rudy Ooms: https://call4cloud.nl/2020/03/how-to-deploy-hkcu-changes-while-blocking-powershell/#part4
 
+# Get information of current user
 $currentUser = (Get-Process -IncludeUserName -Name explorer | Select-Object -First 1 | Select-Object -ExpandProperty UserName).Split("\")[1] 
 
 $Data = $currentUser
@@ -30,14 +31,14 @@ Foreach ($Key in $Keys) {
   IF (($key.GetValueNames() | % { $key.GetValue($_) }) -match "\b$CurrentUser\b" ) { $sid = $key }
 }
 
+# Add SID of current user to a variable
 $sid = $sid.pschildname
 
 New-PSDrive HKU Registry HKEY_USERS | out-null
-
 #endregion
 
-
 #region Variables for sensitivity labels in Adobe Acrobat
+
 $RegKeyPath1 = "HKLM:\SOFTWARE\WOW6432Node\Policies\Adobe\Adobe Acrobat\DC\FeatureLockDown"
 $RegKeyPath2 = "HKU:\$sid\SOFTWARE\Adobe\Adobe Acrobat\DC\MicrosoftAIP"
 
