fa20cd36a0de8485f87b400a92104fbf2579089e
Simon Skotheimsvik <simon.skotheimsvik@gmail.com>
2023-01-13 08:19:31 +0100

    Create Exchange-ListMailUsers.ps1

---
+++ Microsoft/Exchange/Exchange-ListMailUsers.ps1
@@ -0,0 +1,35 @@
+
+<#
+  .NOTES
+  ===========================================================================
+   Created on:   	13.01.2023
+   Created by:   	Simon Skotheimsvik
+   Filename:     	Exchange-ListMailUsers.ps1
+   Instructions:    
+  ===========================================================================
+  
+  .DESCRIPTION
+    This script will list details of all mailboxes on Exchange server
+    
+  .EXAMPLE
+    Exchange-ListMailUsers.ps1.ps1 
+#>
+
+$(Foreach ($mailbox in Get-Recipient -ResultSize Unlimited){
+$Stat = $mailbox | Get-MailboxStatistics | Select TotalItemSize,ItemCount,TotalDeletedItemSize,DeletedItemCount,Servername,Database
+    New-Object PSObject -Property @{
+    FirstName = $mailbox.FirstName
+    LastName = $mailbox.LastName
+    DisplayName = $mailbox.DisplayName
+    TotalItemSize = $Stat.TotalItemSize
+    TotalDeletedItemSize = $Stat.TotalDeletedItemSize
+    ItemCount = $Stat.ItemCount
+    DeletedItemCount = $Stat.DeletedItemCount
+    Servername = $Stat.Servername
+    Database = $Stat.Database
+    PrimarySmtpAddress = $mailbox.PrimarySmtpAddress
+    Alias = $mailbox.Alias
+    EmailAddresses = $mailbox.EmailAddresses
+    RecipientType = $mailbox.RecipientType
+    }
+}) | Select RecipientType,FirstName,LastName,DisplayName,TotalItemSize,ItemCount,TotalDeletedItemSize,DeletedItemCount,Servername,Database,PrimarySmtpAddress,Alias,@{Name=“EmailAddresses”;Expression={$_.EmailAddresses | Where-Object {$_.PrefixString -ceq “smtp”} | ForEach-Object {$_.SmtpAddress}}} | Export-CSV .\Exchange-ListMailUsers.csv -Encoding UTF8 -NoTypeInformation
