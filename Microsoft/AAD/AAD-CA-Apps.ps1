0b4040568a63fab34a45c9059302cdecb30ceb9c
Simon Skotheimsvik <simon.skotheimsvik@gmail.com>
2023-07-04 12:06:24 +0200

    Add two more apps

--- Microsoft/AAD/AAD-CA-Apps.ps1
+++ Microsoft/AAD/AAD-CA-Apps.ps1
@@ -20,6 +20,8 @@ $APPIDs = @(
     "9cdead84-a844-4324-93f2-b2e6bb768d07"  # Azure Virtual Desktop
     "0af06dc6-e4b5-4f28-818e-e78e62d137a5"  # Cloud PC
     "d4ebce55-015a-49b5-a083-c84d1797ae8c"  # Microsoft Intune Enrollment
+    "45a330b1-b1ec-4cc1-9161-9f03992aa49f"  # Universal Store Service APIs and Web Application
+    "a4a365df-50f1-4397-bc59-1a1564b8bb9c"  # Microsoft Remote Desktop
 )
 #endregion Variables
 
