8bc8576bf9566b8a8647c96f3fa969f44c7f0b2c
Simon Skotheimsvik <simon.skotheimsvik@gmail.com>
2024-01-12 10:05:16 +0100

    Autopilot Group tag fixer for Automation Account

---
+++ Microsoft/Intune/Group Tag Fixer/Add-ApplicationPermissionsToSystemAssignedManagedIdentity.ps1
@@ -0,0 +1,31 @@
+<#
+    .SYNOPSIS
+    Created on:     01.06.2023
+    Modified on:    02.06.2023
+    Created by:     CloudWay, Simon Skotheimsvik
+    Info:           CloudWay Services, Evergreen Roadmap, Group Tag Fixer
+    
+    .DESCRIPTION
+    Routine to add application persmissions to system assigned managed identity.
+#>
+
+#region Variables
+$TenantID = "11111111-1111-1111-1111-111111111111"
+$ServicePrincipalId = "22222222-2222-2222-2222-222222222222"
+#endregion Variables
+
+#region Connect to Microsoft Graph
+Connect-MgGraph -TenantId $TenantID -Scopes "Application.Read.All","AppRoleAssignment.ReadWrite.All,RoleManagement.ReadWrite.Directory"
+#endregion connect
+
+#region Set Service Principal Permissions
+$graphStdApp = Get-MgBetaServicePrincipal -Filter "AppId eq '00000003-0000-0000-c000-000000000000'"
+$app_roles = "DeviceManagementServiceConfig.ReadWrite.All", "Device.Read.All"
+$Permissions = $graphStdApp.AppRoles | Where-Object {$_.value -in $app_roles}
+foreach ($Permission in $Permissions) {
+    New-MgBetaServicePrincipalAppRoleAssignment -ServicePrincipalId $ServicePrincipalId -PrincipalId $ServicePrincipalId -AppRoleId $Permission.Id -ResourceId $graphStdApp.Id
+}
+
+#endregion set
+
+# Disconnect-MgGraph
--- Microsoft/Intune/Group Tag Fixer/RB-Grouptag Fixer.ps1
+++ Microsoft/Intune/Group Tag Fixer/RB-Grouptag Fixer.ps1
@@ -1,35 +1,25 @@
 <#
     .SYNOPSIS
     Created on:     01.06.2023
-    Modified on:    05.01.2024
-    Created by:     CloudWay, Simon Skotheimsvik
-    Info:           Group Tag Fixer
-    Version:        1.1
+    Modified on:    12.01.2024
+    Created by:     Simon Skotheimsvik
+    Info:           https://skotheimsvik.no
+    Version:        1.1.1
     
     .DESCRIPTION
-    Function App for fixing Group tag on Windows Autopilot devices where this is missing.
-    This code is setting correct group tag on autopilot computers based on computer name.
-#>
+    Automatic set Group tag on Autopilot devices based on computer name.
+    Script designed for running in Azure Automation Account using managed identity.#>
 
 #region Variables
-$TenantId = Get-AutomationVariable -Name 'tenantid'
-$GroupTagDefinition = "UCC Autopilot"
+$TenantId = Get-AutomationVariable -Name 'aa-no-grouptag-tenantid'
 
 $csvContent = @"
-DevicePrefix;Dept;ScopeTag;GroupTag;AutopilotProfileGuid
-APC-;APC;UCC - APC Managed Devices;UCC-APC;72b2d424-33b4-4cdf-8689-90700c362fc3
-CS-;Computer Science;UCC - Computer Science Managed Devices;UCC-CS;a371eabb-8808-4cca-9058-47b990c59ef0
-CUBS-;Business and Law;UCC - CUBS Managed Devices;UCC-CUBS;da05ac92-c550-4d8b-92cf-54d7dbe3b593
-DENT-;Dentistry;UCC - Dentistry Managed Devices;UCC-DENT;a9cacbfe-304c-4a14-9a00-b19186f5998c
-ENG-;Engineering;UCC - Engineering Managed Devices;UCC-ENG;bb0ddd0d-5a98-40f4-8c0e-407af06bd8c5
-AVMS-;IT Services AVMS;UCC - IT Services AVMS Windows Managed Devices;UCC-AVMS;98dd8ac3-ef2a-455c-9c99-8f41728f45a9
-SIT-;IT Services SIT;UCC - IT Services SIT Windows Managed Devices;UCC-SIT;a517f67e-4344-4b9d-b481-4205c8145f1d
-ITSM-;"IT Services Staff ";UCC - IT Services Windows Managed Devices;UCC-ITSM;" "
-LAW-;Law;UCC - Law Managed Devices;UCC-LAW;ac774473-653c-4860-b6fb-88629989c474
-LIB-;Library;UCC - Library Managed Devices;UCC-LIB;7367e07e-c0d9-4612-8fbe-a5384c654f7a
-MED-;Medicine;UCC - Medicine Managed Devices;UCC-MED;7e3621cc-5957-4f66-a622-b5813f88c2cb
-SONM-;Midwifery;UCC - Nursing and Midwifery Managed Devices;UCC-SONM;f6692965-3675-482e-a7aa-77d8f5a22032
-SEFS-;Science;UCC - SEFS Managed Devices;UCC-SEFS;e75e7c22-8b3a-4726-8de9-5c18bd7c3a87
+DevicePrefix;GroupTag
+SE-;Device-SE
+NO-;Device-NO
+DK-;Device-DK
+FI-;Device-FI
+DE-;Device-DE
 "@
 
 $GroupTagInfo = $csvContent | ConvertFrom-Csv -delimiter ";"
