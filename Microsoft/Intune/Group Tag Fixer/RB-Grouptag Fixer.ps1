6ef3064f4b0b6d2ef594eed17896b1160ee6b1a8
Simon Skotheimsvik <simon.skotheimsvik@gmail.com>
2024-01-16 10:13:56 +0100

    Updated with JSON logging

--- Microsoft/Intune/Group Tag Fixer/RB-Grouptag Fixer.ps1
+++ Microsoft/Intune/Group Tag Fixer/RB-Grouptag Fixer.ps1
@@ -4,7 +4,7 @@
     Modified on:    12.01.2024
     Created by:     Simon Skotheimsvik
     Info:           https://skotheimsvik.no
-    Version:        1.1.2
+    Version:        1.1.3
     
     .DESCRIPTION
     Automatic set Group tag on Autopilot devices based on computer name.
@@ -46,30 +46,52 @@ foreach ($Group in $GroupTagInfo) {
         # Iterate alle devices found where name starting with Prefix
         foreach ($Device in $Devices) {
             # Get variables from the Autopilot device
+            $ReturnBodyTemp = New-Object -TypeName PSObject     # New JSON object for logging
             $DeviceDisplayName = $Device.DisplayName
             $AutopilotZTDID = ($Device.PhysicalIds | Select-String -Pattern '\[ZTDID\]:(.*)').Matches.Groups[1].Value
+            $ReturnBodyTemp | Add-Member -MemberType NoteProperty -Name "DisplayName" -Value "$DeviceDisplayName" -Force    # Add value for logging
+            $ReturnBodyTemp | Add-Member -MemberType NoteProperty -Name "Target Group tag" -Value "$DeviceTargetGroupTag" -Force    # Add value for logging
+            $ReturnBodyTemp | Add-Member -MemberType NoteProperty -Name "ZTDID" -Value "$AutopilotZTDID" -Force    # Add value for logging
             try {
                 $DeviceGroupTag = ($Device.PhysicalIds | Select-String -Pattern '\[OrderId\]:(.*)').Matches.Groups[1].Value
+                $ReturnBodyTemp | Add-Member -MemberType NoteProperty -Name "OrderId" -Value "$DeviceGroupTag" -Force    # Add value for logging
             }
             catch {
                 Write-Error "$($DeviceDisplayName) is missing grouptag."
+                $ReturnBodyTemp | Add-Member -MemberType NoteProperty -Name "OrderId" -Value "-" -Force    # Add value for logging
                 $DeviceGroupTag = ""
             }
 
             # Check if current Group tag is Ok
             if ($DeviceTargetGroupTag -ne $DeviceGroupTag) {
                 Write-Warning "$($DeviceDisplayName) has grouptag ""$($DeviceGroupTag)"" not matching target grouptag ""$($DeviceTargetGroupTag)"". Device will be updated."
+                $ReturnBodyTemp | Add-Member -MemberType NoteProperty -Name "Action" -Value "Wrong Group tag" -Force    # Add value for logging
                 
                 # Update Autopilot device with new Group tag
                 $params = @{
                     groupTag = $DeviceTargetGroupTag
                 }
-                Update-MgBetaDeviceManagementWindowsAutopilotDeviceIdentityDeviceProperty -WindowsAutopilotDeviceIdentityId $AutopilotZTDID -BodyParameter $params
+                try {
+                    Update-MgBetaDeviceManagementWindowsAutopilotDeviceIdentityDeviceProperty -WindowsAutopilotDeviceIdentityId $AutopilotZTDID -BodyParameter $params
+                    $ReturnBodyTemp | Add-Member -MemberType NoteProperty -Name "Status" -Value "Succeed" -Force    # Add value for logging
+                    write-warning "Device $($CurrentDeviceName), Group tag set to $($DeviceTargetGroupTag)"              
+                }
+                catch {
+                    $ReturnBodyTemp | Add-Member -MemberType NoteProperty -Name "Status" -Value "Failed" -Force    # Add value for logging
+                    write-warning "Device $($CurrentDeviceName), Failed setting Group tag to $($DeviceTargetGroupTag)"              
+                }
+
             }
             else {
                 Write-Output "$($DeviceDisplayName) has grouptag ""$($DeviceGroupTag)"". No change required."
+                $ReturnBodyTemp | Add-Member -MemberType NoteProperty -Name "Action" -Value "OK Group tag" -Force    # Add value for logging
+
             }
 
+            #write output logs in JSON format
+            $LogOutputMsg = $ReturnBodyTemp | ConvertTo-Json 
+            Write-Output $LogOutputMsg
+            
             $Device = $null
             $DeviceDisplayName = $null
             $DeviceGroupTag = $null
