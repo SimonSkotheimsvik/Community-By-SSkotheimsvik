2932f5867ef7dc921062a57f629f0d57ff419fef
Simon Skotheimsvik <simon.skotheimsvik@gmail.com>
2024-03-11 12:33:12 +0100

    Updated version

--- Microsoft/AAD/AAD-CA-Apps.ps1
+++ Microsoft/AAD/AAD-CA-Apps.ps1
@@ -1,33 +1,36 @@
 <#
 .SYNOPSIS
-  Script to register Azure Enterprise Applications.
+  Script to register missing Azure Enterprise Applications.
 .DESCRIPTION
-    Script to register Azure Enterprise Applications for use in Azure Conditional Access policies.
+    Script to register missing Azure Enterprise Applications for use in CA policies.
 .EXAMPLE
     
 .NOTES
-    Version:        1.0
+    Version:        1.2
     Author:         CloudWay, Simon Skotheimsvik
     Info:           https://cloudway.com        
     Creation Date: 23.05.2023
     Version history:
     1.0 - (23.05.2023) Script released
+    1.1 - (29.06.2023) Script updated with Universal Store Service APIs and Web Application
+    1.2 - (12.02.2024) Convert to Microsoft Graph PowerShell SDK V2 module, Simon Skotheimsvik
 #>
 
 #region Variables
 # Insert APP IDs for Enterprise Applications to be registered
 $APPIDs = @(
     "9cdead84-a844-4324-93f2-b2e6bb768d07"  # Azure Virtual Desktop
-    "0af06dc6-e4b5-4f28-818e-e78e62d137a5"  # Cloud PC
+    "0af06dc6-e4b5-4f28-818e-e78e62d137a5"  # Windows 365
     "d4ebce55-015a-49b5-a083-c84d1797ae8c"  # Microsoft Intune Enrollment
-    "45a330b1-b1ec-4cc1-9161-9f03992aa49f"  # Universal Store Service APIs and Web Application
+    "45a330b1-b1ec-4cc1-9161-9f03992aa49f"  # Windows Store for Business
     "a4a365df-50f1-4397-bc59-1a1564b8bb9c"  # Microsoft Remote Desktop
+    "9751c96f-923d-43bd-8930-ae900037b2fa"  # MoveAdmin
 )
 #endregion Variables
 
 #region connect
-Select-MgProfile beta
-Connect-MgGraph -Scopes "Application.ReadWrite.All" -ForceRefresh
+# disconnect-mggraph
+Connect-MgGraph -Scopes "Application.ReadWrite.All"
 Import-Module Microsoft.Graph.Applications
 #endregion connect
 
@@ -35,7 +38,7 @@ Import-Module Microsoft.Graph.Applications
 # Register Enterprise applications
 foreach ($ID in $APPIDs) {
     # Finds Enterprise Applications, register it if not existing
-    if ($ExistingApp = Get-MgServicePrincipal -all | Where-Object { $_.AppId -like $ID }) {
+    if ($ExistingApp = Get-MgBetaServicePrincipal -all | Where-Object { $_.AppId -like $ID }) {
         $DisplayName = $ExistingApp.DisplayName
         Write-Output "App with ID ""$ID"" exists as $DisplayName."
     }
@@ -44,7 +47,7 @@ foreach ($ID in $APPIDs) {
         $ServicePrincipalID = @{
             "AppId" = "$ID"
         }
-        New-MgServicePrincipal -BodyParameter $ServicePrincipalId | Format-List id, DisplayName, AppId, SignInAudience
+        New-MgBetaServicePrincipal -BodyParameter $ServicePrincipalId | Format-List id, DisplayName, AppId, SignInAudience
     }
 }
-#endregion script 
+#endregion script
