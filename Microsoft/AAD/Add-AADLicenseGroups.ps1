aca1170b7919f16e87842ab3513de2c3f8ec9f54
Simon Skotheimsvik <simon.skotheimsvik@gmail.com>
2024-04-19 10:57:53 +0200

    New version 1.2

--- Microsoft/AAD/Add-AADLicenseGroups.ps1
+++ Microsoft/AAD/Add-AADLicenseGroups.ps1
@@ -1,9 +1,8 @@
 <#
 .SYNOPSIS
-    Script to create Azure AD License groups.
+    Script to create Azure AD License groups
 .DESCRIPTION
     Script to create Azure AD License groups for available SKUs used for assigning licenses to users in the tenant.
-    The script has a table of SKUs not managed by groups, and a table of translations to apply common abbreviations.
 .EXAMPLE
     
 .NOTES
@@ -13,6 +12,7 @@
     Version history:
     1.0 - (08.11.2022) Script released
     1.1 - (07.07.2023) Adding users with existing licenses to the groups
+    1.2 - (13.02.2023) Convert to Microsoft Graph PowerShell SDK V2 module, Simon Skotheimsvik
 
 #>
 
@@ -30,7 +30,7 @@ function RenameDisplayName {
 #endregion functions
 
 #region Variables
-# Insert SKUs not manageable by groups: Get-MgSubscribedSku | Where-Object { $_.SkuPartNumber -notin $SKUsNotToManage }
+# Insert SKUs not manageable by groups: Get-MgBetaSubscribedSku | Where-Object { $_.SkuPartNumber -notin $SKUsNotToManage }
 $SKUsNotToManage = @(
     "WINDOWS_STORE"
     "RMSBASIC"
@@ -44,8 +44,10 @@ $SKUsNotToManage = @(
 
 # Insert translations used to shorten group names
 $DisplayNameTranslations = @{
-    "Azure Active Directory" = 'AAD'
-    "Microsoft 365"          = 'M365'
+    "Azure Active Directory"         = 'AAD'
+    "Microsoft 365"                  = 'M365'
+    "Office 365"                     = 'O365'
+    "Enterprise Mobility + Security" = 'EMS'
 }
 
 # Import Microsoft CSV file with friendly display name and SKU Partnumber
@@ -63,15 +65,14 @@ $skuHashTable = @{}
 #endregion Variables
 
 #region connect
-Connect-MgGraph -Scopes "Directory.Read.All", "Group.ReadWrite.All" -ForceRefresh
-Select-MgProfile beta
+Connect-MgGraph -Scopes "Directory.Read.All", "Group.ReadWrite.All"
 Import-Module Microsoft.Graph.Groups
 #endregion connect
 
 #region script 
 
 # Gather all SKUs in Tenant
-$SKUsToManage = Get-MgSubscribedSku | Where-Object { $_.SkuPartNumber -notin $SKUsNotToManage }
+$SKUsToManage = Get-MgBetaSubscribedSku | Where-Object { $_.SkuPartNumber -notin $SKUsNotToManage }
 
 # Create groups for SKUs in Tenant
 foreach ($SKU in $SKUsToManage) {
@@ -80,12 +81,12 @@ foreach ($SKU in $SKUsToManage) {
     $GroupDisplayName = RenameDisplayName -DisplayName "AZ-LIC-$(($skuHashTable["$SKUpartno"]).DisplayName)"
 
     # Finds group, create it if not existing
-    if ($Group = Get-MgGroup -All | Where-Object { $_.DisplayName -like $GroupDisplayName }) {
+    if ($Group = Get-MgBetaGroup -All | Where-Object { $_.DisplayName -like $GroupDisplayName }) {
         Write-Output """$GroupDisplayName"" exists"
     }
     else {
         Write-Output """$GroupDisplayName"" does not exist"
-        $Group = New-MgGroup -DisplayName $GroupDisplayName -Description "This group is used to assign $(($skuHashTable["$SKUpartno"]).DisplayName) licenses." -MailEnabled:$false -SecurityEnabled -MailNickName ($GroupDisplayName).Replace(" ", "") #-IsAssignableToRole:$true
+        $Group = New-MgBetaGroup -DisplayName $GroupDisplayName -Description "This group is used to assign $(($skuHashTable["$SKUpartno"]).DisplayName) licenses." -MailEnabled:$false -SecurityEnabled -MailNickName ($GroupDisplayName).Replace(" ", "") #-IsAssignableToRole:$true
     }
 
     # Add license to group
@@ -98,7 +99,7 @@ foreach ($SKU in $SKUsToManage) {
         RemoveLicenses = @(
         )
     }
-    Set-MgGroupLicense -GroupId $Group.Id -BodyParameter $params
+    Set-MgBetaGroupLicense -GroupId $Group.Id -BodyParameter $params
 
     #Get all users with the specified license
     Write-Output "Getting all users with the $(($skuHashTable["$SKUpartno"]).DisplayName) license"
@@ -111,7 +112,7 @@ foreach ($SKU in $SKUsToManage) {
             "@odata.id" = "https://graph.microsoft.com/v1.0/directoryObjects/{$($user.Id)}"
         }
 
-        New-MgGroupMemberByRef -GroupId $Group.Id -BodyParameter $params
+        New-MgBetaGroupMemberByRef -GroupId $Group.Id -BodyParameter $params
     }
 }
 #endregion script 
