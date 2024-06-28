244c0ba7786477af06da9087ad080bdfafeaed75
Simon Skotheimsvik <simon.skotheimsvik@gmail.com>
2024-06-28 09:45:14 +0200

    New alternative solution

---
+++ Microsoft/Intune/PR008 - Start URL On Logon/Alternative 2 (Registry)/StartURLonLogon-Detection.ps1
@@ -0,0 +1,33 @@
+<#
+  .NOTES
+  ===========================================================================
+   Created on:   	26.06.2024
+   Created by:   	Simon Skotheimsvik
+   Filename:     	StartURLonLogon-Detection.ps1
+   Info:          https://skotheimsvik.no 
+  ===========================================================================
+  
+  .DESCRIPTION
+    This remediation package adds URL to start on logon.
+    
+#>
+
+$RegContent = @"
+RegKeyPath,Key,Value,Type
+"HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run","SimonDoes","explorer https://skotheimsvik.no","String"
+"@
+
+$RegData = $RegContent | ConvertFrom-Csv -delimiter ","
+
+foreach ($Reg in $RegData) {
+    $ExistingValue = (Get-Item -Path $($Reg.RegKeyPath)).GetValue($($Reg.Key))
+    if ($ExistingValue -ne $($Reg.Value)) {
+      Write-Host $($Reg.Key) "Not Equal"
+      Exit 1
+      Exit #Remediation 
+    }
+    else {
+#      Write-Host $($Reg.Key) "Equal"
+    }
+}
+Exit 0
---
+++ Microsoft/Intune/PR008 - Start URL On Logon/Alternative 2 (Registry)/StartURLonLogon-Remediation.ps1
@@ -0,0 +1,44 @@
+<#
+  .NOTES
+  ===========================================================================
+   Created on:   	26.06.2024
+   Created by:   	Simon Skotheimsvik
+   Filename:     	StartURLonLogon-Detection.ps1
+   Info:          https://skotheimsvik.no 
+  ===========================================================================
+  
+  .DESCRIPTION
+    This remediation package adds URL to start on logon.
+    
+#>
+
+$RegContent = @"
+RegKeyPath,Key,Value,Type
+"HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run","SimonDoes","explorer https://skotheimsvik.no","String"
+"@
+
+$RegData = $RegContent | ConvertFrom-Csv -delimiter ","
+
+foreach ($Reg in $RegData) {
+
+    IF (!(Test-Path ($Reg.RegKeyPath))) {
+        Write-Host ($Reg.RegKeyPath) " does not exist. Will be created."
+        New-Item -Path $($Reg.RegKeyPath) -Force | Out-Null
+    }
+    
+    IF ((Get-ItemProperty -Path $Reg.RegKeyPath -Name $Reg.Key -ErrorAction SilentlyContinue) -eq $null) {
+        Write-Host "$($Reg.Key) does not exist. Will be created."
+        New-ItemProperty -Path $($Reg.RegKeyPath) -Name $($Reg.Key) -Value $($Reg.Value) -PropertyType $($Reg.Type) -Force
+    }
+    
+    $ExistingValue = (Get-Item -Path $($Reg.RegKeyPath)).GetValue($($Reg.Key))
+    if ($ExistingValue -ne $($Reg.Value)) {
+        Write-Host "$($Reg.Key) not correct value. Will be set."
+        Set-ItemProperty -Path $($Reg.RegKeyPath) -Name $($Reg.Key) -Value $($Reg.Value) -Force
+    }
+    else {
+        Write-Host "$($Reg.Key) is correct"
+    }
+}
+
+Exit 0
