4d07769e3be47360f740f8ab7d334a9e01572284
Simon Skotheimsvik <simon.skotheimsvik@gmail.com>
2024-10-08 12:35:46 +0200

    Version 1.0.5 of the remediation

--- Microsoft/Intune/PR009 - Autopatch NoAutoUpdate/Autopatch-NoAutoUpdate-Remediation.ps1
+++ Microsoft/Intune/PR009 - Autopatch NoAutoUpdate/Autopatch-NoAutoUpdate-Remediation.ps1
@@ -4,13 +4,13 @@
    Created by:    Simon Skotheimsvik
    Filename:      Autopatch-NoAutoUpdate-Remediation.ps1
    Info:          https://skotheimsvik.no 
-   Version:       1.0.4
+   Version:       1.0.5
   
   .DESCRIPTION
     This remediation script checks if the registry key exists and holds a different value than expected. If the value is different, the script will clean up the registry key.
 #>
 
-$RegKeyPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
+$RegKeyPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate"
 $RegKey = "NoAutoUpdate"
 $ExpectedValue = 0
 
@@ -22,8 +22,15 @@ try {
                 Write-Host "Registry key does not exist. No remediation needed."
                 Exit 0 # No changes required
             } elseif ($ExistingValue -ne $ExpectedValue) {
+                # Stop the Windows Update service
+                Stop-Service -Name wuauserv -Force
+
                 Set-ItemProperty -Path $RegKeyPath -Name $RegKey -Value $ExpectedValue -ErrorAction SilentlyContinue
                 Write-Host "Set key: $RegKey in path $RegKeyPath to the expected value $ExpectedValue."
+
+                # Start the Windows Update service
+                Start-Service -Name wuauserv
+
                 Exit 1 # Remediation successful
             } else {
                 Write-Host "$($RegKey) in path $($RegKeyPath) equals $($ExistingValue). No remediation needed."
