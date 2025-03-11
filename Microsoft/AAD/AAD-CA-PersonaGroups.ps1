ca925949ae5b0d6ea405ee23ca22b918c0753263
Simon Skotheimsvik <simon.skotheimsvik@gmail.com>
2025-03-11 11:41:35 +0100

    Updated version

--- Microsoft/AAD/AAD-CA-PersonaGroups.ps1
+++ Microsoft/AAD/AAD-CA-PersonaGroups.ps1
@@ -1,63 +1,66 @@
 <#
 .SYNOPSIS
-  Script to create Azure CA Persona groups.
+  Script to create Entra ID CA Persona groups.
 .DESCRIPTION
-    Script to create Azure CA Persona groups used for assigning users to Personas targeting Conditional Access policies.
+    Script to create role-assignable Entra ID CA Persona groups used for assigning users to Personas targeting Conditional Access policies.
 .EXAMPLE
     
 .NOTES
-    Version:        1.0
-    Author:         CloudWay, Simon Skotheimsvik
-    Info:           https://cloudway.com        
-    Creation Date: 23.05.2023
-    Version history:
-    1.0 - (23.05.2023) Script released
-
+    Author:         Simon Skotheimsvik
+    Info:           https://skotheimsvik.no        
+    Versions:
+                1.0.0 - (23.05.2023) Script released
+                1.0.1 - (12.03.2024) Convert to Microsoft Graph PowerShell SDK V2 module, Simon Skotheimsvik
+                1.0.2 - (10.09.2024) Added new CA Persona for Service Accounts, Simon Skotheimsvik
+                1.0.3 - (10.09.2024) Bugfix in finding existing groups, Simon Skotheimsvik
 #>
 
 #region Variables
-# Insert groups to be created
+# Insert groups to be created. Security defined after ";"
 $GroupNames = @(
-    "AZ-Persona-CA-BreakGlassAccounts"
-    "AZ-Persona-CA0000-Global BaseProtection Exclusions"
-    "AZ-Persona-CA0100-Admin Users"
-    "AZ-Persona-CA0200-Standard Users"
-    "AZ-Persona-CA0300-Front Line Users"
-    "AZ-Persona-CA0400-Shared Users"
-    "AZ-Persona-CA0500-Service Accounts"
-    "AZ-APP-CA-Deviation-Salesforce"
-    "AZ-APP-CA-Deviation-idPowerToys"
+    "AZ-Persona-CA-BreakGlassAccounts;secure"
+    "AZ-Persona-CA-Admins;secure"
+    "AZ-Persona-CA-AzureServiceAccounts;secure"
+    "AZ-Persona-CA-Exclude-Block LegacyAuth;secure"
+    "AZ-Persona-CA-Global-BaseProtection-Exclusions;secure"
+    "AZ-Persona-CA-Internals;normal"
 )
 $Date = Get-Date -Format "yyyy.MM.dd"
 #endregion Variables
 
 #region connect
-Select-MgProfile beta
-Connect-MgGraph -Scopes "Directory.Read.All", "Group.ReadWrite.All", "RoleManagement.ReadWrite.Directory" -ForceRefresh
-Import-Module Microsoft.Graph.Groups
+Connect-MgGraph -Scopes "Directory.Read.All", "Group.ReadWrite.All", "RoleManagement.ReadWrite.Directory"
+#Install-Module Microsoft.Graph.Beta.Groups
+Import-Module Microsoft.Graph.Beta.Groups
 #endregion connect
 
 #region script 
 # Create groups in Tenant if not existing
 foreach ($Group in $GroupNames) {
     # Create description
-    $GroupDescription = $Group.Split("-")[-1]
-    $GroupType = $Group.Split("-")[1]
-    if ($GroupType -like "Persona") {
-        $Description = "This group is used to assign user account to the CA Persona: $GroupDescription - $Date, CloudWay."
-    }
-    elseif ($GroupType -like "APP") {
-        $Description = "This group is used to assign user account to CA APP Deviation for the app $GroupDescription - $Date, CloudWay."
-    }
-    $MailNickName = $grouptype+"."+($GroupDescription).Replace(" ", "")
+    $GroupName = $Group.Split(";")[0]
+    $GroupSecurity = $Group.Split(";")[1]
+        if ($GroupSecurity -like "secure") { $IsAssignableToRole = $true }
+        else { $IsAssignableToRole = $false }
 
-    # Finds group, create it if not existing
-    if ($ExistingGroup = Get-MgGroup | Where-Object { $_.DisplayName -like $Group }) {
-        Write-Output """$Group"" exists"
+    $GroupDescription = $GroupName.Split("-")[-1]
+    $GroupType = $GroupName.Split("-")[1]
+        if ($GroupType -like "Persona") {
+            $Description = "This group is used to assign user account to the CA Persona: $GroupDescription - $Date, CloudWay."
+        }
+        elseif ($GroupType -like "APP") {
+            $Description = "This group is used to assign user account to CA APP Deviation for the app $GroupDescription - $Date, CloudWay."
+        }
+    $MailNickName = $grouptype + "." + (($GroupDescription).Replace(" ", "")).Replace("(Hybrid)", "Hybrid")
+
+    # Find group, create it if not existing
+
+    if ($ExistingGroup = Get-MgBetaGroup -Filter "DisplayName eq '$($GroupName)'") {
+        Write-Output """$GroupName"" exists"
     }
     else {
-        Write-Output """$Group"" does not exist. Will be created now."
-        $NewGroup = New-MgGroup -DisplayName $Group -Description $Description -MailEnabled:$false -SecurityEnabled -MailNickName $MailNickName -IsAssignableToRole # $true -SecurityEnabled $true -Visibility "Hidden"
+        Write-Output """$GroupName"" does not exist. Will be created now as $GroupSecurity."
+        $NewGroup = New-MgBetaGroup -DisplayName $GroupName -Description $Description -MailEnabled:$false -MailNickName $MailNickName -SecurityEnabled -IsAssignableToRole:$IsAssignableToRole
     }
 }
 #endregion script 
