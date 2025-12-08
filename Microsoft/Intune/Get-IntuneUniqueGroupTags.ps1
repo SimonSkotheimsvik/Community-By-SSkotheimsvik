3b85e8758e6b339a9eed9976ddf3f6a508c41e52
simon.skotheimsvik@gmail.com <simon.skotheimsvik@gmail.com>
2025-12-08 15:07:11 +0100

    Added some more numbers

--- Microsoft/Intune/Get-IntuneUniqueGroupTags.ps1
+++ Microsoft/Intune/Get-IntuneUniqueGroupTags.ps1
@@ -39,4 +39,16 @@ $groupTagCounts = $devices | Group-Object -Property GroupTag | Sort-Object Name
 Write-Host "`nGroup Tag Counts:"
 $groupTagCounts | ForEach-Object { Write-Host "$($_.Name): $($_.Count)" }
 
+# Display different enrollmentstates for Autopilot devices.
+$devices | Group-Object -Property EnrollmentState | Select-Object Name, Count
+
+# Display numbers for enrollment states for each grouptag
+$devices | Group-Object -Property GroupTag, EnrollmentState | Select-Object @{Name='GroupTag';Expression={$_.Values[0]}}, @{Name='EnrollmentState';Expression={$_.Values[1]}}, Count | Sort-Object GroupTag, EnrollmentState | Format-Table -AutoSize
+
 $devices | measure
+
+$devices[3] | fl
+
+# Get number of devices assigned to each Autopilot profile
+Import-Module Microsoft.Graph.Beta.DeviceManagement.Enrollment
+$profileAssignments = @(); foreach ($profile in $profiles) { $assignedDevices = Get-MgBetaDeviceManagementWindowsAutopilotDeploymentProfileAssignedDevice -WindowsAutopilotDeploymentProfileId $profile.Id -All; $profileAssignments += [PSCustomObject]@{ ProfileName = $profile.DisplayName; DeviceCount = ($assignedDevices | Measure-Object).Count } }; $profileAssignments | Format-Table -AutoSize
